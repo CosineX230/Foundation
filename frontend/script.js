4b980d7d83bd524f8f8a4a3ed4c6e3cf2ab75c7e
CosineX230 <23zabarsky.isaac@gmail.com>
2026-09-19 22:37:24 -0400

    fixed delimiters

--- frontend/script.js
+++ frontend/script.js
@@ -5,6 +5,7 @@ require("tau-prolog/modules/lists.js")
 let session = pl.create(1000)
 
 const prologCode = fs.readFileSync("../backend/level1.pl", "utf8");
+
 function test() {
     session.consult(prologCode, {
         success: () => {
@@ -13,16 +14,16 @@ function test() {
                     let results = [];
 
                     session.answers(answer => {
-                        if(answer !== false) {
+                        if (answer !== false) {
                             results.push(session.format_answer(answer));
                         } else {
                             console.log("Query results:", results);
                         }
-                    };
+                    });
                 },
                 error: err => console.error("Query error: ", err)
-            };
-        }),
+            });
+        },
         error: err => console.error("Consult error:", err)
     });
 }
