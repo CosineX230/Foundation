d22ae38cafd7bd1f9f7b491a08b410be4f5c04a1
CosineX230 <23zabarsky.isaac@gmail.com>
2026-09-20 09:57:14 -0400

    Begin html and prolog content

--- backend/level1.pl
+++ backend/level1.pl
@@ -2,12 +2,45 @@ material(fire).
 material(water).
 material(plant).
 material(stone).
+material(air).
 
-material(blade).
+tool(blade).
+tool(wood).
 
 concept(science).
 concept(technology).
 concept(life).
 
+combine(fire, water) :- material(air).
+combine(fire, plant) :- material(air).
+combine(fire, stone) :- material(blade).
+combine(fire, air) :- material(fire).
+combine(fire, blade) :- material(blade).
+combine(fire, wood) :- material(coal).
+
+combine(water, fire) :- material(air).
+combine(water, plant) :- material(tree).
+combine(water, stone) :- material(stone).
+combine(water, air) :- material(water).
+combine(water, blade) :- material(stone).
+combine(water, wood) :- material(wood).
+
+combine(plant, fire) :- material(air).
+combine(plant, water) :- material(tree).
+combine(plant, stone) :- material(plant).
+combine(plant, air) :- material(tree).
+combine(plant, blade) :- material(wood).
+combine(plant, wood) :- material(tree).
+
+combine(stone, fire) :- material(blade).
+combine(stone, water) :- material(stone).
+combine(stone, plant) :- material(plant).
+combine(stone, air) :- material()
+
+
+
+
+
 combine(technology, stone) :- material(blade).
 combine(technology, fire) :- material(torch).
+combine()
